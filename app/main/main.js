ec53616e9120be5010925017b3e12f8237b31f85
Alexey Shulga <levsha128@gmail.com>
2017-08-03 13:28:00 +0300

    Add menu in page

--- app/main/main.js
+++ app/main/main.js
@@ -2,13 +2,37 @@
 
 angular.module('myApp.main', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/main', {
-    templateUrl: 'main/main.html',
-    controller: 'MainCtrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/main', {
+      templateUrl: 'main/main.html',
+      controller: 'MainCtrl'
+    });
+  }])
 
-.controller('MainCtrl', [function() {
-
-}]);
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.menu = [
+      {
+        name: 'margarita',
+        ingredients: ['basil', 'tomato', 'mozzarella'],
+        price: 5.75
+      },
+      {
+        name: 'peperoni',
+        ingredients: ['peperoni', 'tomato', 'mozzarella', 'parmejano'],
+        price: 7.00
+      },
+      {
+        name: 'meat',
+        ingredients: ['bacon', 'tomato', 'mozzarella', 'ham', 'salami'],
+        price: 12.00
+      },
+      {
+        name: 'marina',
+        ingredients: ['shrimp', 'parmejano', 'tuna', 'galric'],
+        price: 15.75
+      }
+    ];
+    $scope.menu.map(function (pizza, index) {
+      pizza.id = index + 1;
+    });
+  }]);
