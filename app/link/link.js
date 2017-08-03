dbddb6f92aa3c20550c3b2a7c66968da0d07e672
Alexey Shulga <levsha128@gmail.com>
2017-08-03 12:43:53 +0300

    Highlight active nav link

---
+++ app/link/link.js
@@ -0,0 +1,19 @@
+angular.module('myApp.link', []). //thanks to https://stackoverflow.com/a/12631074
+directive('activeLink', ['$location', function (location) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs, controller) {
+      var clazz = attrs.activeLink;
+      var path = attrs.href;
+      path = path.substring(2); //hack because path does not return including hashbang
+      scope.location = location;
+      scope.$watch('location.path()', function (newPath) {
+        if (path === newPath) {
+          element.addClass(clazz);
+        } else {
+          element.removeClass(clazz);
+        }
+      });
+    }
+  };
+}]);
